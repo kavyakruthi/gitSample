41e04e5e61d64d54ac2b43d461cb5be171ac59a1
Kavya M <kavya.madaiah@softvision.com>
2019-05-29 16:30:38 +0530

    final commit

--- GitSample/src/main/java/GitSample/Apps.java
+++ GitSample/src/main/java/GitSample/Apps.java
@@ -4,6 +4,9 @@ public class Apps {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10, b=20;
+		int c= a+b;
+		System.out.print("c"+c);
 		System.out.print("Softvision BTAT Assessment");
 
 	}
